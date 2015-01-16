93f49f2ca31386db2df29cd10f22317ee5793976
Vernon Wong <vrnnwng@gmail.com>
2015-01-15 21:26:48 -0800

    EVERYTHING v.2
    Title.

---
+++ Assets/Scripts/Hover.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Hover : MonoBehaviour {
+	
+	public bool translateable;
+	
+	public Color OnMouseOverColor = Color.yellow;
+	public Font OnMouseOverFont;
+	public Font OnMouseExitFont;
+	
+	void Start(){
+		if (translateable){
+			GetComponent<TextMesh>().color = OnMouseOverColor;
+		}
+	}
+	
+	void OnMouseOver(){
+		GetComponent<TextMesh>().font = OnMouseOverFont;
+	}
+	
+	void OnMouseExit(){
+		GetComponent<TextMesh>().font = OnMouseExitFont;
+	}
+
+}
