8bd8520e8c315a6e798aec2f3e60ed258c836ed8
frgperez <frgperez@ucsc.edu>
2015-01-30 02:13:22 -0800

    Added note zoom function script + more goodies
    Added all of the assets and whatnot that I used. That way you can just
    go to town without having to set anything up for my scripts.

---
+++ Assets/Scripts/noteZoom.cs
@@ -0,0 +1,45 @@
+﻿/*
+ * Requires a sprite that's set up with at least two animations. Let's the animation run for about 10 seconds (or whatever 10.0f is) and then loads 
+ * another scene.
+ */
+
+using UnityEngine;
+using System.Collections;
+
+public class noteZoom : MonoBehaviour {
+	private Animator animator;
+	bool animationPlaying = false;
+	float timeLeft = 10.0f;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (animationPlaying == true) {
+			//print(timeLeft);
+			timeLeft -= Time.deltaTime;
+
+			if(timeLeft <= 0){
+				Application.LoadLevel ("workInProgress");
+			}
+
+		}
+	}
+
+	void OnMouseDown(){
+		//print ("hey");
+
+		if (animationPlaying == false) {
+			animator.SetInteger("spriteState", 1);
+			animationPlaying = true;
+		}
+		/*else{
+			animator.SetInteger("spriteState", 0);
+			animationPlaying = false;
+		}*/
+	}
+
+}
