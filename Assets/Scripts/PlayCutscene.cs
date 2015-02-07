9e4eb1eb8a5eeddc11b191d4b4cc31d5b2491cdf
Vernon Wong <vrnnwng@gmail.com>
2015-02-06 23:33:33 -0800

    Scrolling Cutscene Implemented
    PlayCutscene script attached to button. Scrolling direction/speed can be
    adjusted from the object inspector.
    
    Several unnecessary prefabs deleted.

---
+++ Assets/Scripts/PlayCutscene.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayCutscene : MonoBehaviour {
+
+    public Camera mainCamera;
+    public Camera cutsceneCamera;
+    public float xDirection;
+    public float yDirection;
+    public float zDirection;
+
+    private bool playingCutscene = false;
+    private Vector3 direction;
+	
+    void Start() {
+        mainCamera.enabled = true;
+        cutsceneCamera.enabled = false;
+
+        direction = new Vector3(xDirection, yDirection, zDirection);
+    }
+
+
+    void onMouseDown()
+    {
+
+    }
+
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Clicked");
+            playingCutscene = true;
+            mainCamera.enabled = false;
+            cutsceneCamera.enabled = true;
+
+        }
+
+        if (playingCutscene)
+        {
+            cutsceneCamera.transform.position += direction*Time.deltaTime;
+        }
+	}
+}
