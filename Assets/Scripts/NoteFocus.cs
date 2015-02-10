df06dd87e3b2e5e84389aa29f5a2abe200bba415
Vernon Wong <vrnnwng@gmail.com>
2015-02-09 17:28:50 -0800

    Sheet Tweened for Zoom - Complete
    Paper now completely tweened to focus/unfocus on mouse clicks.

--- Assets/Scripts/NoteFocus.cs
+++ Assets/Scripts/NoteFocus.cs
@@ -11,25 +11,71 @@ public class NoteFocus : MonoBehaviour {
 
     private bool scrollDown;
     private bool isOffScreen;
+    private bool mouseOver;
+    private bool focused;
 
     private Vector3 defaultNotePos;
     private Vector3 defaultCameraPos;
+    private Vector3 cameraFront;
 
     void Start()
     {
         defaultNotePos = transform.position;
         defaultCameraPos = mainCamera.transform.position;
-    }
+        defaultCameraPos += new Vector3(0, 0, 10);
+        cameraFront = defaultCameraPos;
+        Debug.Log("defaultCameraPos = " + defaultCameraPos);
 
+        focused = false;
+    }
+    
     public void OnMouseDown()
     {
         Debug.Log("OnClick");
-        StartCoroutine(ScrollDown());
+        StartCoroutine(Focus());
     }
 
-    IEnumerator ScrollDown()
+    public void OnMouseEnter()
     {
-        yield return StartCoroutine(HOTween.To(transform, 0.2f, "position", new Vector3(0,-10,0), true).WaitForCompletion());
-        //transform.position -= new Vector3(0.0f,-0.5f, 0.0f);
+        // Toggles mouseover state while not in focused mode
+        if (!focused)
+        {
+            mouseOver = true;
+        }
+    }
+
+    public void OnMouseExit()
+    {
+        // Toggles mouseover state while not in focused mode
+        if (!focused)
+        {
+            mouseOver = false;
+        }
+    } 
+
+    public void Update()
+    {
+        // Detects clicks off the object
+        if (Input.GetMouseButtonDown(0) && !mouseOver)
+        {
+            Debug.Log("OffClick");
+            StartCoroutine(Unfocus());
+        }
+    }
+
+    // Coroutine called when focusing onto a note
+    IEnumerator Focus()
+    {
+        yield return StartCoroutine(HOTween.To(transform, 0.8f, "rotation", new Vector3(0, 0, 0), false).WaitForCompletion());
+        yield return StartCoroutine(HOTween.To(transform, 0.6f, "position", cameraFront, false).WaitForCompletion());
+        focused = true;
+    }
+
+    // Coroutine called when unfocusing away from a note
+    IEnumerator Unfocus()
+    {
+        yield return StartCoroutine(HOTween.To(transform, 0.6f, "position", defaultNotePos, false).WaitForCompletion());
+        yield return StartCoroutine(HOTween.To(transform, 0.8f, "rotation", new Vector3(80, 0, 0), false).WaitForCompletion());
+        focused = false;
     }
 }
