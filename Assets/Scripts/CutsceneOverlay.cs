3ac086ff8738970499721ff06f2e031d2115cfb2
Vernon Wong <vrnnwng@gmail.com>
2015-04-03 16:58:57 -0700

    Cutscene Assets
    Title.

---
+++ Assets/Scripts/CutsceneOverlay.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CutsceneOverlay : MonoBehaviour {
+
+    public GameObject c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
