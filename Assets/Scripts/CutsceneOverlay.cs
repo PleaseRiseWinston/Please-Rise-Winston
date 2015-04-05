e0cfa2b0afb6a1a528b2eea266806e44dacf2f98
Vernon Wong <vrnnwng@gmail.com>
2015-04-05 16:58:37 -0700

    General Cleanup + Cutscene Overlay/Polish
    Reorganized many assets. Removed MM sprites. Changed many 3D plane
    assets to 2D sprites.
    
    Cutscene module added to handle the text overlay. Should scale to screen
    size in build. Heck, whole game should now (not tested).

--- Assets/Scripts/CutsceneOverlay.cs
+++ Assets/Scripts/CutsceneOverlay.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using Holoville.HOTween;
 using UnityEngine.EventSystems;
 using System.Collections;
@@ -6,13 +7,51 @@ using System.Collections.Generic;
 
 public class CutsceneOverlay : MonoBehaviour {
 
+    public GameObject cameraController;
+    public PlayCutscene playCutscene;
+
+    public GameObject[] overlays;
+    private Color transparent;
+    private Color solid;
+
+    private float waitTime;
+
 	// Use this for initialization
 	void Start () {
-        GameObject[] overlays = new GameObject[transform.childCount];
+        // Reference CameraController
+        cameraController = GameObject.Find("CameraController");
+        playCutscene = cameraController.GetComponent<PlayCutscene>();
+
+        // Declarations for alpha states
+        transparent = new Color(1f, 1f, 1f, 0f);
+        solid = new Color(1f, 1f, 1f, 1f);
+        waitTime = 2f;
+
+        // Insert all children planes into array and make transparent
+        overlays = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            overlays[i] = transform.GetChild(i).gameObject;
+            overlays[i].GetComponent<Image>().color = transparent;
+            Debug.Log(overlays[i].name);
+        }
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    public void RunOverlay()
+    {
+        for (int i = 0; i < transform.childCount; i++)
+        {
+            StartCoroutine(FadeText(overlays[i].transform, 5f * i));
+        }
+    }
+
+    IEnumerator FadeText(Transform text, float stallTime)
+    {
+        Debug.Log("Fading: " + text.name);
+        yield return new WaitForSeconds(stallTime);
+        StartCoroutine(HOTween.To(text.GetComponent<Image>(), 0.5f, "color", solid, false).WaitForCompletion());
+        yield return new WaitForSeconds(5f);
+        StartCoroutine(HOTween.To(text.GetComponent<Image>(), 0.5f, "color", transparent, false).WaitForCompletion());
+    }
 }
+
--- Assets/Scripts/PlayCutscene.cs
+++ Assets/Scripts/PlayCutscene.cs
@@ -11,12 +11,11 @@ public class PlayCutscene : MonoBehaviour {
     public Camera mainCamera;
     public Camera cutsceneCamera;
 
+    public GameObject overlayCanvas;
+    public CutsceneOverlay cutsceneOverlay;
+
     public Vector3 cutsceneCamPos;
     public float cutsceneTime;
-
-    public float xDirection;
-    public float yDirection;
-    public float zDirection;
 
     public bool playingCutscene = false;
 
@@ -24,17 +23,16 @@ public class PlayCutscene : MonoBehaviour {
     private Vector3 direction;
 
     public AudioClip cutsceneMusic;
-    public GameObject c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11;
 	
     void Start() {
         gameCamera.enabled = false;
         mainCamera.enabled = true;
         cutsceneCamera.enabled = false;
 
-        cutsceneTime = 6.0f;
+        overlayCanvas = GameObject.Find("CutsceneOverlay");
+        cutsceneOverlay = overlayCanvas.GetComponent<CutsceneOverlay>();
 
-        // Add other checkpoints for camera to hit here
-        //camPoints[0] = new Vector3(0, 0, 0);
+        cutsceneTime = 6.0f;
     }
 
 	void Update () {
@@ -43,8 +41,10 @@ public class PlayCutscene : MonoBehaviour {
 
     public IEnumerator Play()
     {
+        playingCutscene = true;
         Debug.Log("Entering Play");
-        print(gameObject.name);
+
+        cutsceneOverlay.RunOverlay();
 
         gameObject.AddComponent<AudioSource>();
         AudioSource cutsceneAudio = gameObject.GetComponent<AudioSource>();
@@ -56,38 +56,20 @@ public class PlayCutscene : MonoBehaviour {
         cutsceneAudio.Play();
         Transform camTransform = cutsceneCamera.transform;
 
-        //Instantiate(c1, cutsceneCamera.transform.position + (cutsceneCamera.transform.forward * 5), cutsceneCamera.transform.rotation);
-        //HOTween.To(c1.GetComponent<MeshRenderer>().sharedMaterial, 1f, "color", Color.white);
-        //print("first text");
-        //new WaitForSeconds(6);
-        //HOTween.To(c1.GetComponent<MeshRenderer>().sharedMaterial, 1f, "color", Color.clear);
-        //HOTween.To(c2.GetComponent<MeshRenderer>().sharedMaterial, 1f, "color", Color.white);
-        //print("second text");
-        //new WaitForSeconds(6);
-        //HOTween.To(c2.GetComponent<MeshRenderer>().sharedMaterial, 1f, "color", Color.clear);
-        yield return StartCoroutine(HOTween.To(camTransform, 0f, "position", new Vector3(150f, 100f, 0), true).WaitForCompletion());
-        HOTween.To(GameObject.Find("Cutscene1-1").GetComponent<MeshRenderer>().sharedMaterial, 3f, "color", Color.clear, false);
-        yield return StartCoroutine(HOTween.To(camTransform, 0f, "position", new Vector3(-100f, 50f, 0), true).WaitForCompletion());
-        HOTween.To(GameObject.Find("Cutscene1-2").GetComponent<MeshRenderer>().sharedMaterial, 3f, "color", Color.clear, false);
-        yield return StartCoroutine(HOTween.To(camTransform, 0f, "position", new Vector3(100f, 20f, 0), true).WaitForCompletion());
-        HOTween.To(GameObject.Find("Cutscene1-3").GetComponent<MeshRenderer>().sharedMaterial, 3f, "color", Color.clear, false);
-        yield return StartCoroutine(HOTween.To(camTransform, 0f, "position", new Vector3(50f, -180f, 0), true).WaitForCompletion());
+        yield return StartCoroutine(HOTween.To(camTransform, 20f, "position", new Vector3(150f, 100f, 0), true).WaitForCompletion());
+        HOTween.To(GameObject.Find("Cutscene1-1").GetComponent<SpriteRenderer>(), 3f, "color", Color.clear, false);
+        yield return StartCoroutine(HOTween.To(camTransform, 10f, "position", new Vector3(-100f, 50f, 0), true).WaitForCompletion());
+        HOTween.To(GameObject.Find("Cutscene1-2").GetComponent<SpriteRenderer>(), 3f, "color", Color.clear, false);
+        yield return StartCoroutine(HOTween.To(camTransform, 10f, "position", new Vector3(100f, 20f, 0), true).WaitForCompletion());
+        HOTween.To(GameObject.Find("Cutscene1-3").GetComponent<SpriteRenderer>(), 3f, "color", Color.clear, false);
+        yield return StartCoroutine(HOTween.To(camTransform, 17f, "position", new Vector3(70f, -180f, 0), true).WaitForCompletion());
 
         cutsceneAudio.Stop();
+        playingCutscene = false;
         //SwitchToGame();
         Vector3 gameCameraPos = gameCamera.transform.position;
         mainCamera.transform.position = gameCameraPos;
         SwitchToMain();
-
-
-        /*
-        // Visits each camera point to hit and pauses at each
-        foreach (Vector3 point in camPoints)
-        {
-            StartCoroutine(HOTween.To(cutsceneCamera, 6.0f, "position", point).WaitForCompletion());
-            //new WaitForSeconds(2.0f);
-        }
-        */
     }
 
     // TODO: Add fade sequences at beginning and ends of each of these.
