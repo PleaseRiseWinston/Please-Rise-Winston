ab3613c57cf5c3127de223caa6c29b068acc1699
frgperez <frgperez@ucsc.edu>
2015-05-11 04:32:10 -0700

    Papers need to be fully translated now.

--- Assets/Scripts/Tray.cs
+++ Assets/Scripts/Tray.cs
@@ -66,7 +66,7 @@ public class Tray : MonoBehaviour
         
 
         // Only trays/moves curNote iff paper is unfocused and the last paper has arrived at its destination
-        if (!gameControllerScript.curNote.GetComponent<PaperScript>().focused && !gameControllerScript.curNoteInMotion && trayCooldown <= Time.time)
+        if (!gameControllerScript.curNote.GetComponent<PaperScript>().focused && !gameControllerScript.curNoteInMotion && trayCooldown <= Time.time && allTranslated)
         {
             //print("curNote name: " + gameControllerScript.curNote.name);
             gameControllerScript.curNote.GetComponent<PaperScript>().inTray = true;
